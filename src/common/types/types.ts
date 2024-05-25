b7c00b8ad160750789121b643cda43e3effe8d26
Eugen Vorobiov <jeka.vorobjov@gmail.com>
2024-05-25 17:00:12 +0300

    feat(core): add store, TreeList processing

---
+++ src/common/types/types.ts
@@ -0,0 +1,27 @@
+export interface ItemsListResponseNode {
+    cat?: ItemsListResponseNode | ItemsListResponseNode[],
+    leaf?: ItemsListResponseNode[],
+    "_nodeId": string,
+    "_name": string
+}
+
+
+export interface ItemsListResponse {
+    tree: {
+        "cat": ItemsListResponseNode,
+        "_treeId": string
+    }
+}
+
+export interface TreeListItem {
+    id: string;
+    name: string;
+    children: TreeListItem[];
+    isSelected: boolean;
+    treeCollapsed: boolean;
+}
+
+export interface ListItem {
+    id: string;
+    name: string;
+}
